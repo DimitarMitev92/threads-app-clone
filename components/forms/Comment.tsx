ae2574c4bed230f1cd708c7188284cb78b870cad
Dimitar Mitev <d.mitev920123@gmail.com>
2023-08-07 18:17:46 +0300

    Reply comment

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -18,6 +18,7 @@ import { usePathname, useRouter } from 'next/navigation';
 // import { updateUser } from '@/lib/actions/user.actions';
 import { CommentValidation } from '@/lib/validations/thread';
 import Image from 'next/image';
+import { addCommentToThread } from '@/lib/actions/thread.action';
 // import { createThread } from '@/lib/actions/thread.action';
 
 interface Props {
@@ -38,14 +39,13 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
     });
 
     const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
-        // await createThread({
-        //     text: values.thread,
-        //     author: userId,
-        //     communityId: null,
-        //     path: pathname,
-        // });
-
-        router.push('/');
+        await addCommentToThread(
+            threadId,
+            values.thread,
+            JSON.parse(currentUserId),
+            pathname
+        );
+        form.reset();
     };
 
     return (
